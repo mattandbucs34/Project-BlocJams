8d094f9dd61ff2311d66b01109554c623ef43b82
mattandbucs34 <35737262+mattandbucs34@users.noreply.github.com>
2018-07-03 21:19:17 -0600

    add files Play Bar Checkpoint

--- bloc-jams-react/src/components/Album.js
+++ bloc-jams-react/src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
 class Album extends Component {
     constructor(props) {
@@ -47,11 +48,19 @@ class Album extends Component {
       }
     }
 
+    handlePrevClick() {
+      const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+      const newIndex = Math.max(0, currentIndex - 1);
+      const newSong = this.state.album.songs[newIndex];
+      this.setSong(newSong);
+      this.play();
+    }
+
     iconFunction(song, index) {
       if (this.state.isPlaying && this.state.currentSong === song) {
-        return <span className="ion-md-pause"></span>
+        return <span className='ion-md-pause'></span>
       }else if (this.state.isHovered === index + 1){
-        return <span className="ion-md-play-circle"></span>
+        return <span className='ion-md-play-circle'></span>
       }else {
         return <span>{index + 1}</span>
       }
@@ -59,26 +68,26 @@ class Album extends Component {
 
     render() {
         return (
-            <section className = "album">
-              <section id="album-info">
-                <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title} />
-                <div className="album-details">
-                  <h1 id="album-title">{this.state.album.title}</h1>
-                  <h2 className="artist">{this.state.album.artist}</h2>
-                  <div id="release-into">{this.state.album.releaseInfo}</div>
+            <section className = 'album'>
+              <section id='album-info'>
+                <img id='album-cover-art' src={this.state.album.albumCover} alt={this.state.album.title} />
+                <div className='album-details'>
+                  <h1 id='album-title'>{this.state.album.title}</h1>
+                  <h2 className='artist'>{this.state.album.artist}</h2>
+                  <div id='release-into'>{this.state.album.releaseInfo}</div>
                 </div>
               </section>
-              <table id="song-list">
+              <table id='song-list'>
                 <colgroup>
-                  <col id="song-number-column" /> 
-                  <col id="song-title-column" />
-                  <col id="song-duration-column" />
+                  <col id='song-number-column' /> 
+                  <col id='song-title-column' />
+                  <col id='song-duration-column' />
                 </colgroup>
               
                 <tbody>
                   {
                     this.state.album.songs.map( (song, index) =>
-                      <tr className="song" key={index} 
+                      <tr className='song' key={index} 
                         onClick={() => this.handleSongClick(song)}
                         onMouseEnter={() => this.setState({ isHovered: index + 1 })}
                         onMouseLeave={() => this.setState({ isHovered: null })}
@@ -88,12 +97,17 @@ class Album extends Component {
                         </td>
                         <td>{song.title}</td>
                         <td>{song.duration}</td>
-                        <td>{this.audioElement.src}</td>
                       </tr>
                     )
                   }
                 </tbody>
               </table>
+              <PlayerBar 
+                isPlaying={this.state.isPlaying} 
+                currentSong={this.state.currentSong}
+                handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+                handlePrevClick={() => this.handlePrevClick()}
+               />
             </section>
         );
     }
