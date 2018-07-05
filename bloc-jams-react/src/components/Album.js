27821d39319d722aeef37c327bdc7263f9cf0183
mattandbucs34 <35737262+mattandbucs34@users.noreply.github.com>
2018-07-05 16:29:51 -0600

    update #4 Play Bar: Range Input Assignment

--- bloc-jams-react/src/components/Album.js
+++ bloc-jams-react/src/components/Album.js
@@ -92,7 +92,17 @@ class Album extends Component {
     }
 
     formatTime(time) {
+      const floorTime = Math.floor(time);
+      const minutes = Math.floor(floorTime / 60);
+      const seconds = (floorTime % 60);
 
+      if(isNaN(time)) {return "-:--"}
+
+      if(seconds < 10 ){
+        return minutes + ":0" + seconds;
+      }else
+        return minutes + ":" + seconds;
+        
     }
 
     iconFunction(song, index) {
@@ -135,7 +145,7 @@ class Album extends Component {
                           {this.iconFunction(song,index)}
                         </td>
                         <td>{song.title}</td>
-                        <td>{song.duration}</td>
+                        <td>{this.formatTime(song.duration)}</td>
                       </tr>
                     )
                   }
@@ -149,6 +159,7 @@ class Album extends Component {
                 handleSongClick={() => this.handleSongClick(this.state.currentSong)}
                 handlePrevClick={() => this.handlePrevClick()}
                 handleNextClick={() => this.handleNextClick()}
+                formatTime={(e) => this.formatTime(e)}
                 handleTimeChange={(e) => this.handleTimeChange(e)}
                />
             </section>
